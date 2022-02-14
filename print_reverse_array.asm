afd8c6d2d27f800c2118b7844331f1ea663118fc
Huizhong Xue <hxue@cs.ucsb.edu>
2022-02-14 15:23:56 -0800

    print_reverse_array.asm

--- print_reverse_array.asm
+++ print_reverse_array.asm
@@ -6,10 +6,30 @@
 .data
 	array: .word 1 2 3 4 5 6 7 8 9 10
 	cout: .asciiz "The contents of the array in reverse order are:\n"
+	newline: .asciiz "\n"
 
 .text
 printA:
-    # TODO: Write your function code here
+    move $t0, $a0 # array
+	move $t1, $a1 # array length
+	li $t4, 4 # 4 bytes
+	mult $t1, $t4
+	mflo $t3
+	add $t0, $t0, $t3
+	addi $t0, $t0, -4
+	for: beq $t1, $zero, end_for
+	lw $t2, 0($t0)
+	addi $t0, $t0, -4
+	li $v0, 1
+	move $a0, $t2
+	syscall
+	li $v0, 4
+	la $a0, newline
+	syscall
+	addi $t1, $t1, -1
+	j for
+
+	end_for: jr $ra
 
 main:
 	li $v0, 4
@@ -22,5 +42,6 @@ main:
 	jal printA
 
 exit:
-	# TODO: Write code to properly exit a SPIM simulation
+	li $v0, 10
+	syscall
 
--- test.cpp
+++ test.cpp
@@ -1,7 +1,7 @@
 #include <iostream>
 using namespace std;
 
-int main(){
+/*int main(){
     int x = 5;
     int y = 7;
     int z = 0;
@@ -15,4 +15,17 @@ int main(){
         x++;
     }
     cout << z << endl;
+}*/
+
+void printA(int a[], int al){
+for (int i = al-1; i >=0; i--)
+std::cout << a[i] << "\n";
+}
+// again you don’t need to implement main
+int main(){
+int arr[10] = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
+int alength = 10;
+std::cout << "The contents of the array in reverse order are:\n";
+printA(arr, alength);
+// Expected Output : 10 9 8 7 6 5 4 3 2 1
 }
